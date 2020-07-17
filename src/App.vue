908aca71d57f0f15caf525a92ff3d26b85bdde19
yuancheng <yuancheng@inboyu.com>
2020-07-17 16:36:36 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png"/>
+    <ClipLoader :size="'50px'"/>
+    <DatePicker />
+    <CarouselWide :slidesToShow="3">
+      <template v-slot:slides>
+        <img class="fluid" src="http://placehold.it/2000x1000" alt />
+        <img class="fluid" src="http://placehold.it/2000x1000" alt />
+        <img class="fluid" src="http://placehold.it/2000x1000" alt />
+        <img class="fluid" src="http://placehold.it/2000x1000" alt />
+        <img class="fluid" src="http://placehold.it/2000x1000" alt />
+      </template>
+    </CarouselWide>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import ClipLoader from '@bit/greyby.vue-spinner.clip-loader';
+import DatePicker from '@bit/myliang.fish-ui.styles.date-picker';
+import CarouselWide from '@bit/hbs.vue.carousel-wide';
 
 export default {
   name: 'App',
+  components: {ClipLoader, DatePicker, CarouselWide},
   mounted() {
     axios.get('/api/json', {
       params: {}
